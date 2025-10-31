95202d166c72bf532ababeac7e6a0adfb7ded190
Angel Gustavo Sanchez Rodas <asanchezr12@miumg.edu.gt>
2025-10-31 13:05:52 -0600

    Primer commit del proyecto SistemaRRHH

---
+++ Recursos-Humanos-Spring/src/main/java/gm/rh/controlador/EmpleadoControlador.java
@@ -0,0 +1,23 @@
+package gm.rh.controlador;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("rh-app")
+@CrossOrigin(value = "http://localhost:3000")
+public class EmpleadoControlador {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(EmpleadoControlador.class);
+
+    
+
+
+
+
+
+}
