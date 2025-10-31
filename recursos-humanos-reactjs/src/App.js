0f7058326c9b94e632db178ef031776e11525293
Angel Gustavo Sanchez Rodas <asanchezr12@miumg.edu.gt>
2025-10-31 15:55:26 -0600

    Se agrego una barra de navegacion

--- recursos-humanos-reactjs/src/App.js
+++ recursos-humanos-reactjs/src/App.js
@@ -1,8 +1,13 @@
+import ListadoEmpleados from "./empleados/ListadoEmpleados";
+import Navegacion from "./plantilla/Navegacion";
+
 function App() {
   return (
-    <div class = "container text-center">
-      <h3>Sistema de Recursos Humanos</h3>
+    <div className="container">
+      <Navegacion/>
+    <ListadoEmpleados/>
     </div>
+    
   );
 }
 
